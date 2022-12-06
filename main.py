9b00c04b0ca94e39c7cf6873b0a25d9d48f5f833
Cho <mcho1@babson.edu>
2022-12-06 10:17:24 -0500

    main

--- main.py
+++ main.py
@@ -16,14 +16,26 @@ def compound_int(P, r, n, t):
     print("Final value will be", value)
 
 def compound_int_prompts():
-    principle = int(input('Enter initial amount: '))
-    rate = (float(input("Enter expected interest rate (%) ")))/100
-    n_input = (input('Enter compounding period (daily, monthly, or annual): '))
-    if n_input == 'annual':
+    principle = int(input('Enter how much you would like to invest: '))
+    #rate = (float(input("Enter expected interest rate (%) ")))/100
+    # What if the input chose the name of the stock one wants to invest in and based on the choice, the rate for that stick is used when
+    # the code runs
+    r_input = (input('From the following options, enter which stock you would like to invest in: VOO, QQQ, VTI,or SPY: ')) 
+    if r_input == 'VOO' or 'voo':
+        rate = 10/100
+    elif r_input == 'QQQ' or 'qqq':
+        rate = 13/100
+    elif r_input == 'VTI' or 'vti':
+        rate = 9.79/100
+    elif r_input == 'SPY' or 'spy':
+        rate = 10/100
+    
+    n_input = (input('Enter your desired compounding period: Daily, monthly, or annually: '))
+    if n_input == 'annually' or 'Annually' or 'ANNUALLY':
         frequency = 1 
-    elif n_input == 'monthly':
+    elif n_input == 'monthly' or 'Monthly' or 'MONTHLY':
         frequency = 12
-    elif n_input == 'daily':
+    elif n_input == 'daily' or 'Daily' or 'MONTHLY':
         frequency = 365
     time = float(input('Enter years of investment: '))
 
